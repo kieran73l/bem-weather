3b38da63f28c88766a8931ea7eb09628a14992da
Rafael Sangali <rafaelsangali@outlook.com>
2024-04-04 10:25:52 -0300

    feat(setup): setup libs and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const MODULES = [BrowserModule, AppRoutingModule, HttpClientModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: MODULES,
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+const PAGES = [HomeComponent];
+
+@NgModule({
+  declarations: PAGES,
+  imports: [CommonModule],
+  exports: PAGES,
+})
+export class PagesModule { }
