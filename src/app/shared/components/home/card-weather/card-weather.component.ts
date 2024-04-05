df274d247256037013f2928b13ade45d82b170bd
Rafael Sangali <rafaelsangali@outlook.com>
2024-04-05 12:58:40 -0300

    feat(card-weather): adding sky info

--- src/app/shared/components/home/card-weather/card-weather.component.ts
+++ src/app/shared/components/home/card-weather/card-weather.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
 import {
+  faCloud,
+  faDroplet,
   faTemperatureHigh,
   faTemperatureLow,
   faTemperatureQuarter,
+  faWind,
 } from '@fortawesome/free-solid-svg-icons';
 import { WeatherConditions } from 'src/app/interfaces/weather-conditions';
 
@@ -16,8 +19,12 @@ export class CardWeatherComponent {
   @Input() loading = false;
 
   constructor() {}
+  skeletons: number[] = Array(6).map((x, i) => i);
 
   faTemperatureHigh = faTemperatureHigh;
   faTemperatureLow = faTemperatureLow;
   faTemperatureQuarter = faTemperatureQuarter;
+  faDroplet = faDroplet;
+  faCloud = faCloud;
+  faWind = faWind;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   openWeatherUrl: 'https://api.openweathermap.org',
   openWeatherKey: '70b0d6922d6795f25ead92757cc0fb51',
 };
